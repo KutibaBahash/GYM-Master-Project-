59ee55cc6acc33634c868811208eb0beb9cf84e8
Omar Sobh <98892376+OmarSobh@users.noreply.github.com>
2024-08-03 15:19:25 +0300

    server

--- Applaction Backend/server.js
+++ Applaction Backend/server.js
@@ -23,8 +23,9 @@ app.use("/gym-manager", gymManagerRoutes); // gym manager routes
 
 
 
-const PORT =  3000;
+const PORT = process.env.PORT || 3000;
 console.log(PORT);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
